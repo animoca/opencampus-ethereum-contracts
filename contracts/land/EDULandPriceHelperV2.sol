f76f8554161a7a4f8182a1d6cecc2f4e16dad957
gigichan <gigichan@animocabrands.com>
2025-03-19 12:32:53 -0400

    Update calculatePrice to return a constant

--- contracts/land/EDULandPriceHelperV2.sol
+++ contracts/land/EDULandPriceHelperV2.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 import {IEDULandPriceHelper} from "./interfaces/IEDULandPriceHelper.sol";
 
 /// @title EDULandPriceHelperV2
-/// @notice A helper contract to calculate the rental price of a EDULand based on the total effective rental time.
+/// @notice A helper contract to return the EDULand rental price.
 contract EDULandPriceHelperV2 is IEDULandPriceHelper {
     /// @inheritdoc IEDULandPriceHelper
-    function calculatePrice(uint256 totalOngoingRentalTime) external pure returns (uint256) {
-        return Math.max(1000, Math.log2(totalOngoingRentalTime / 15000000000) * 17000);
+    function calculatePrice(uint256) external pure returns (uint256) {
+        return 400;
     }
 }
